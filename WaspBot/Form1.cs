a26f79273074183aebc621c22c692c61b3975018
CLARENCE\john.jamison <jackofthejamison@hotmail.com>
2021-04-13 16:56:52 +0100

    Got it reading from a html file as embedded resource

--- WaspBot/Form1.cs
+++ WaspBot/Form1.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+
+using System.IO;
+using System.Reflection;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,11 +17,23 @@ namespace WaspBot
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
             Xpcom.Initialize("Firefox");
-            geckoWebBrowser1.LoadHtml("<html><body> <style> body{background:#fff} </style> <h1>Hello!!!</h1></body></html>");
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "WaspBot.app.html";
+            string appHTML;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                appHTML = reader.ReadToEnd();
+            }
+
+            geckoWebBrowser1.LoadHtml(appHTML, null);
         }
     }
 }
